08b735a33b8e48fffb30243f80873a87d2d65d2e
Tania Melissa Henandez Narvaez <ilkalhn@gmail.com>
2017-05-25 16:59:51 -0500

    se agrega clase base para los tests

---
+++ common/tests/base.py
@@ -0,0 +1,18 @@
+from rest_framework.test import APIClient
+from tenant_schemas.test.cases import FastTenantTestCase
+from tenant_schemas.test.client import TenantClient
+from test_plus.test import TestCase
+
+
+class BaseClient(TenantClient, APIClient):
+    """Cliente base para las pruebas."""
+
+    pass
+
+
+class BaseTestCase(FastTenantTestCase, TestCase):
+    """Clase base para pruebas unitarias."""
+
+    def _pre_setup(self):
+        super()._pre_setup()
+        self.client = BaseClient(self.tenant)
