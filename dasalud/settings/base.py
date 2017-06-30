53652205cfd62078e599a0aae1628958f4a9bc16
Tania Melissa Henandez Narvaez <ilkalhn@gmail.com>
2017-06-30 15:47:28 -0500

    se coloca como obligatorio estar logueado

--- dasalud/settings/base.py
+++ dasalud/settings/base.py
@@ -199,5 +199,6 @@ LOGGING = {
 
 # Restframework configuration
 REST_FRAMEWORK = {
-    'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',)
+    'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
+    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAuthenticated',)
 }
