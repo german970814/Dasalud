09f0d2dd7e4da9e2016b0a7ba26cf28cf0c3d775
Tania Melissa Henandez Narvaez <ilkalhn@gmail.com>
2017-07-15 11:11:25 -0500

    se agrega anexos a la historia clinica. Falta hacerlo funcional y que se permitan ve o descargar los anexos

--- historias/views.py
+++ historias/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Historia, Formato
+from . import serializers
 
-# Create your views here.
+
+class FormatoView(generics.ListCreateAPIView):
+
+    serializer_class = serializers.FormatoSerializer
+    queryset = Formato.objects.all()
+
+    def post(self, request, *args, **kwargs):
+        print(request.data)
+        return super().post(request, *args, **kwargs)
