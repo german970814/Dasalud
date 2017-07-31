9d9a1104c4dccb437151208be1ea6c1eb20179b2
Tania Melissa Henandez Narvaez <ilkalhn@gmail.com>
2017-07-31 16:13:01 -0500

    avances para permitir sesiones con los servicios

--- organizacional/urls.py
+++ organizacional/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^medicos/$', views.ListarMedicosView.as_view(), name='medicos'),
+    url(r'^sucursales/$', views.ListarSucursalesView.as_view(), name='sucursales'),
 ]
--- organizacional/views.py
+++ organizacional/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 from rest_framework import generics
-from .serializers import EmpleadoSerializer
-from .models import Empleado
+from .serializers import EmpleadoSerializer, SucursalSerializer
+from .models import Empleado, Sucursal
 
 
 class ListarMedicosView(generics.ListAPIView):
-    """Permite listar planes."""
+    """Permite listar los medicos."""
 
     serializer_class = EmpleadoSerializer
     queryset = Empleado.objects.medicos()
     filter_fields = ['instituciones']
+
+
+class ListarSucursalesView(generics.ListAPIView):
+    """Permite listar las sucursales."""
+
+    serializer_class = SucursalSerializer
+    queryset = Sucursal.objects.all()
